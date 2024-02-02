4b3b8aac3e477451467becbd9760a14ee015d1df
WhatsApp API for developers <care@whapi.cloud>
2024-02-02 15:29:12 +0700

    Change hook url in config file.

--- public/config.php
+++ public/config.php
@@ -8,6 +8,6 @@ return [
     "group" => '120363167596599603@g.us',
     // The ID of the product we will send for the example. Create a product in your WhatsApp and find out the product ID: https://whapi.readme.io/reference/getproducts
     "product" => '6559353560856703',
-    // Bot`s URL - Link to your server. At ( {server link}/messages ), when POST is requested, processing occurs in index.php
-    "botUrl" => "https://yoursite.com/messages"
+    // Bot`s URL - Link to your server. At ( {server link}/hook ), when POST is requested, processing occurs in index.php
+    "botUrl" => "https://yoursite.com/hook"
 ];
